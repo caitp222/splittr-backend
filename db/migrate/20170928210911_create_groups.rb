52755071394dd037021765460d8abada5c4aad2b
Caitlin Prentke & Enrico De Santis & Rebecca trachtenberg <pair+caitp222.ed13f.rebeccarose123@devbootcamp.com>
2017-09-28 17:15:33 -0400

    Generate group model and migration

---
+++ db/migrate/20170928210911_create_groups.rb
@@ -0,0 +1,12 @@
+class CreateGroups < ActiveRecord::Migration[5.1]
+  def change
+    create_table :groups do |t|
+      t.string :group_name, null: false
+      t.text :details
+      t.references :creator, foreign_key: true
+      t.boolean :settled_up, default: false
+
+      t.timestamps
+    end
+  end
+end
