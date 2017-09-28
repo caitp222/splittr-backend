19b8a0a94dec2c3ab413e266ad121f4637921146
Caitlin Prentke & Enrico De Santis & Rebecca trachtenberg <pair+caitp222.ed13f.rebeccarose123@devbootcamp.com>
2017-09-28 18:38:22 -0400

    Create users#create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,25 @@ class UsersController < ApplicationController
   end
 
   def new
-
+    User.new
   end
 
   def create
-
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to_user_path(user)
+    else
+      render json: user.errors.full_messages
   end
 
   def find
 
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 
 end
