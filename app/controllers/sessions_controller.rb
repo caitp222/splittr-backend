b9114b33d3dc0e24848d8ad4a6ed9830dbad2e94
Caitlin Prentke <caitlin.prentke@gmail.com>
2017-10-01 11:33:02 -0400

    Change selection to match params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
+      puts session[:user_id]
       render json: user
     else
       render json: {errors: "login failed."}
